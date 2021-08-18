78d4231e922a5172376a45a8bda5052ad0f73477
Yoon Hajung <49065638+YHaJung@users.noreply.github.com>
2021-08-18 16:25:47 +0900

    Create Q2_Hajung.py

---
+++ week1/Q2_Hajung.py
@@ -0,0 +1,49 @@
+def solution(clothes):
+    
+    #count types
+    types = [one_clothes[1] for one_clothes in clothes]
+    types = set(types)
+    clothes_dict = {type:0 for type in types}
+    
+    for one_clothes in clothes:
+        clothes_dict[one_clothes[1]] += 1
+    
+    #calculate cases
+    answer = 1
+    for typecount in clothes_dict.values():
+        answer *= typecount+1
+    answer -= 1
+    
+    return answer
+
+
+'''
+테스트 1 〉	통과 (0.02ms, 10.2MB)
+테스트 2 〉	통과 (0.02ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.2MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.01ms, 10.1MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.01ms, 10.2MB)
+테스트 9 〉	통과 (0.01ms, 10.2MB)
+테스트 10 〉	통과 (0.01ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.3MB)
+테스트 12 〉	통과 (0.02ms, 10.2MB)
+테스트 13 〉	통과 (0.01ms, 10.2MB)
+테스트 14 〉	통과 (0.01ms, 10.2MB)
+테스트 15 〉	통과 (0.01ms, 10.2MB)
+테스트 16 〉	통과 (0.01ms, 10.3MB)
+테스트 17 〉	통과 (0.01ms, 10.2MB)
+테스트 18 〉	통과 (0.01ms, 10.2MB)
+테스트 19 〉	통과 (0.01ms, 10.2MB)
+테스트 20 〉	통과 (0.01ms, 10.1MB)
+테스트 21 〉	통과 (0.01ms, 10.2MB)
+테스트 22 〉	통과 (0.01ms, 10.2MB)
+테스트 23 〉	통과 (0.01ms, 10.2MB)
+테스트 24 〉	통과 (0.01ms, 10.2MB)
+테스트 25 〉	통과 (0.01ms, 10.2MB)
+테스트 26 〉	통과 (0.02ms, 10.2MB)
+테스트 27 〉	통과 (0.01ms, 10.3MB)
+테스트 28 〉	통과 (0.02ms, 10.2MB)
+'''
